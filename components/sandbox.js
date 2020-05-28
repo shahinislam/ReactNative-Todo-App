ce6a0b13ff954723689f44d808c6890dbdde5fc6
Shahinur Islam <shahin.sec.cse@gmail.com>
2020-05-28 23:22:52 +0600

    first commit

---
+++ components/sandbox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Sandbox() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.boxOne}>one</Text>
+            <Text style={styles.boxTwo}>two</Text>
+            <Text style={styles.boxThree}>three</Text>
+            <Text style={styles.boxFour}>four</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        //flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        paddingTop: 40,
+        backgroundColor: '#ddd',      
+    },
+    boxOne: {
+        backgroundColor: 'violet',
+        padding: 10,
+    },
+ 
+    boxTwo: {
+        backgroundColor: 'gold',
+        padding: 20,
+    },
+ 
+    boxThree: {
+        backgroundColor: 'coral',
+        padding: 30,
+    },
+   
+    boxFour: {
+        backgroundColor: 'skyblue',
+        padding: 40,
+    },
+});
